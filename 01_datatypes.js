d140e4d31f96b51b48cf43c4bfda52a5270470ad
Aditya Soni <154958305+Adityasony69@users.noreply.github.com>
2024-02-17 20:03:33 +0530

    Add files via upload

---
+++ 01_datatypes.js
@@ -0,0 +1,33 @@
+// some common keywords used in Java Script as newer version
+
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello!") we use this command to flash notificaton on the screen 
+
+// Code Redability should be high
+console.log(3 + 3);
+
+console.log("Aditya");
+
+// DataTypes  NNBBSSU=Null Number Boolean Bigint Symbol String Undefined
+let name="Aditya"
+let age ="18"
+let isloggedin = false
+let string ="hello"
+// let bigint
+// let symbol = unique
+// undefined = value is to be assigned
+
+
+// Object 
+console.log(typeof null);
+// it will give output as object 
+                // but
+console.log(typeof undefined);
+//  it will give output as object 
+
+
+
+
+
+

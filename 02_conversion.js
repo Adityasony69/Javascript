6541bdb99a4f1684f997d85396557abc562bd60e
Aditya Soni <154958305+Adityasony69@users.noreply.github.com>
2024-02-18 12:51:50 +0530

    Add files via upload

---
+++ 02_conversion.js
@@ -0,0 +1,43 @@
+// CONVERSION IN JAVA SCRIPT
+
+//  Conversion only worked bby using capital letters for datatypes....
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+// Value in number helps to convert the datatype of value in any form with the help of Datatypes
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // if we take string with nmber in let value it will still print number 
+console.log(valueInNumber); // if we take strings in  let value then it will print NAN 
+
+
+        // INVESTIGATION STUDY
+// "33" => 33
+// "33abc"=> NaN
+//  TRUE => 1 ;FALSE => 0;
+
+
+let isloggedin = 1
+let booleanIsLoggedin = Boolean(isloggedin)
+console.log(typeof booleanIsLoggedin);
+console.log( booleanIsLoggedin);
+
+
+
+        // INVESTIGATION STUDY
+// 1=> true ; 0=> true
+//  "" => False
+// "Aditya" => tRUE
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+
+
+
+ 
